90f4663188cded87ffeee3dafdfac88d75ba8325
Inacio Nery <inacio.nery@liferay.com>
2017-09-13 22:03:05 -0300

    Add logging using aspect

---
+++ src/main/java/com/inacionery/basic/config/LoggingAspectConfiguration.java
@@ -0,0 +1,17 @@
+package com.inacionery.basic.config;
+
+import com.inacionery.basic.logging.LoggingAspect;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
+}
